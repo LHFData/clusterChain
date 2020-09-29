c7a3337355a77e415845f5f6de9d8109b794caa7
Liohoifung <liaohaifeng@bupt.edu.cn>
2020-09-29 19:09:49 +0800

    基本区块链，

---
+++ cmd/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"./coin"
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	bc := coin.NewBlockchain()
+	bc.AddBlock("send 1 BTC To me")
+	bc.AddBlock("send 2 more to WY")
+	for _, block := range bc.Blocks {
+		fmt.Printf("prev.hash:%x\n", block.PrevBlockHash)
+		fmt.Printf("Data:%s\n", block.Data)
+		fmt.Printf("Hash:%x\n", block.Hash)
+		pow := coin.NewProofOfWork(block)
+		fmt.Printf("Pow:%s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+
+	}
+
+}
